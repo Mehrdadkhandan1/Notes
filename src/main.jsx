bb1bb09956c18bdceeb10c5e36f8feda197d2ca9
AliDlt <alidlt1@yahoo.com>
2023-12-23 10:49:30 +0330

    change some styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import axios from 'axios'
-import { ContextNoteProvider, LoadingContextProvider } from './context/context.jsx'
-import {  AlertContextProvider } from './components/Alert/Alert.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+import {
+  ContextNoteProvider,
+  LoadingContextProvider,
+} from "./context/context.jsx";
+import { AlertContextProvider } from "./components/Alert/Alert.jsx";
 
 // axios contfig
 
-axios.defaults.baseURL = 'http://localhost:3000/'
+axios.defaults.baseURL = "http://localhost:3000/";
 
-
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AlertContextProvider>
       <LoadingContextProvider>
         <ContextNoteProvider>
           <App />
-        </ContextNoteProvider>,
+        </ContextNoteProvider>
       </LoadingContextProvider>
     </AlertContextProvider>
   </BrowserRouter>
-)
+);
