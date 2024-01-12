5cc1b85babacd92c7d64834901791157e2e611d9
Mehrdadkhandan1 <1Mehrdadkhandan1@gmail.com>
2024-01-12 06:05:42 +0330

    search Completed

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import style from './Search.module.css'
 import { BiSearchAlt2 } from 'react-icons/bi'
 import axios from 'axios'
+import { sliceText } from '../../tools/functions'
+import { Link } from 'react-router-dom'
 const Search = () => {
     const [valueSearch, setValueSearch] = useState('')
     const [data, setData] = useState([])
@@ -12,9 +14,11 @@ const Search = () => {
         const contoroller = new AbortController()
         const fetchD = async () => {
             axios.get(`/api/searchNote/${valueSearch}`, { signal: contoroller.signal }).then(resp => {
+                console.log(resp.data.data)
                 setData(resp.data.data)
             }).catch(err => {
-                console.log(err)
+                setData([])
+
             })
         }
         fetchD()
@@ -35,11 +39,39 @@ const Search = () => {
                 <BiSearchAlt2 />
             </label>
             <input value={valueSearch} onChange={searchContent} placeholder='Search Notes...' type="search" name="search" id="search-input" />
-            <div className={style.resultSeatch}>
-                <div className={style.resultItem}>
-                    
-                </div>
-            </div>
+
+            {
+                valueSearch &&
+                <>
+                    <div className={style.resultSeatch}>
+                        <div className={style.resultItem}>
+                            {data.length ? data.map(res => {
+                                return (
+                                    <div className={style.itemsResult}>
+                                        <h5 className={style.titleSearchResult}>
+                                            <Link to={`/showNote/${res._id}`}> {res.title} </Link>
+                                        </h5>
+
+                                        <div className={style.contentResult}>
+                                            {
+                                                res.content ?
+                                                    <p dangerouslySetInnerHTML={{ __html: sliceText(res.content, 30) }} />
+                                                    :
+                                                    <p>
+                                                        no Content
+                                                    </p>
+                                            }
+                                        </div>
+                                    </div>
+                                )
+                            }) : <p className={style.noResult}>
+                                No results
+                            </p>}
+                        </div>
+                    </div>
+
+                </>
+            }
         </div>
     )
 }
